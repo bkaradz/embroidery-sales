d09e7133278862701f48c2f442544c8e73dcc0c0
karadz <karadz@gmail.com>
2025-02-16 06:07:44 +0200

    WIP removed postcss config

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,20 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter()
+  },
+  vitePlugin: {
+    inspector: {
+      toggleKeyCombo: 'alt-x',
+      showToggleButton: 'always',
+      toggleButtonPos: 'bottom-right'
+    }
+  }
 };
 
 export default config;
